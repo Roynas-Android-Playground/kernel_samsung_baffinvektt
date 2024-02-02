45e128ed2ab4330a08fb88ff03f520b960391cd7
Soo Hwan Na <whiteshell2544@naver.com>
2024-02-02 23:17:14 +0900

    build_kernel: Switch to using out/ directory

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-androideabi-
 export PATH=$PWD/toolchain/bin:$PATH
+COMMON="O=out"
 
+mkdir -p out
 make $COMMON baffinvektt_00_defconfig
 make $COMMON savedefconfig
-cp defconfig arch/arm/configs/baffinvektt_00_defconfig
+cp out/defconfig arch/arm/configs/baffinvektt_00_defconfig
 make $COMMON -j$(nproc)
